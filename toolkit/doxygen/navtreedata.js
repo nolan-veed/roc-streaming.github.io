4b9c9e5bb8ccbd11c85713bb4afbb074e1e08426
nolan-veed <88709630+nolan-veed@users.noreply.github.com>
2024-03-24 17:29:08 +0000

    Deploying to content from @ nolan-veed/roc-streaming.github.io@e537371fb806cf2353153b14c657bd03d1d423e3 🚀

---
+++ toolkit/doxygen/navtreedata.js
@@ -0,0 +1,83 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Roc Toolkit internal modules", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"addr__family_8h.html",
+"classroc_1_1audio_1_1Frame.html#a2f07e5318d2b156a3b27a69dcdb53dbd",
+"classroc_1_1core_1_1Hashmap.html#a28936a4b61b97c992d5b35de59c64238",
+"classroc_1_1core_1_1StringList.html#a8d310d58248bcb86097cfaa47853a574",
+"classroc_1_1netio_1_1NetworkLoop.html#a933e5bfc0354b3c89ced65019cbed5ae",
+"classroc_1_1pipeline_1_1ReceiverLoop.html",
+"classroc_1_1rtcp_1_1Session.html#ae9a4370edad3c4c25d95db4ce20d9936",
+"classroc_1_1sdp_1_1MediaDescription.html#aa11e2fc5fdfeb0f98e28b5eb9dd599dd",
+"dir_ffb3bebc680de4b17ca224c7dabeb28f.html",
+"ireader_8h.html",
+"namespaceroc_1_1core.html#a355041ac5a55a86a07bc556c815caa13",
+"ownership__policy_8h_source.html",
+"roc__rtcp_2headers_8h.html#a992aa446632a37ec1a25f358493ceaaaa8a06f106949370c6d07f658d01dfd4fa",
+"structroc_1_1core_1_1ListNode_1_1ListNodeData.html#a35d4f716cec9a61b8f10b3af5d5def52",
+"target__posix__pc_2roc__core_2console_8h.html#aeac9abf6d607db030dc632f6f3d66fffa763b00d646da1c5d8b8fa56c095bc6f9"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
